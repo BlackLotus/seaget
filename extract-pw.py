e1e012d224f24a3d4e2c0a9548e20b92192b2590
Thomas Schneider <maxmusterm@gmail.com>
2013-10-15 18:43:37 +0200

    Added the pw extractor needs still some work

---
+++ extract-pw.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+import re
+import sys
+import binascii
+
+f=binascii.hexlify(open(sys.argv[1],'r').read())
+pwsR=re.compile('00000000edfe0d90ffff.+?00000c54........(................................................................)(................................................................)')
+pws=pwsR.findall(f)
+for pw in pws[0]:
+    print pw
+    print pw.decode("hex")
